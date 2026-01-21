c75eb98aff0c785f7c54ccb2c3176c7b9b37ee6b
Evgeniia Brezhe <for.zhenya.popova@gmail.com>
2026-01-21 18:37:58 +0100

    terraform variables

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.6.0"
     }
   }
 }
 
 provider "google" {
-  project     = "project-231c402f-f9ad-427f-b80"
-  region      = "us-central1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "project-231c402f-f9ad-427f-b80-terra-bucket"
-  location      = "US"
-  force_destroy = true
+  name                        = var.gcs_bucket_name
+  location                    = var.location
+  force_destroy               = true
   uniform_bucket_level_access = true
 
   lifecycle_rule {
@@ -26,4 +26,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
